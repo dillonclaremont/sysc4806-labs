a5fb6f084fe3ef1cfc20682baa09dc1efb9d2513
dillonclaremont <dillon.claremont@icloud.com>
2020-02-14 11:49:06 -0500

    add travis

---
+++ src/test/java/TestingWebAppTest.java
@@ -0,0 +1,27 @@
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import AddressBook.WebLauncher;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(classes= {WebLauncher.class})
+@AutoConfigureMockMvc
+public class TestingWebAppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void createAddressBook() throws Exception {
+        this.mockMvc.perform(post("/addressBook")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(post("/addBuddy").param("id", "1").param("name", "ThomasTheBrykenator").param("phonenumber", "42069")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/addressBook").param("id", "1")).andDo(print()).andExpect(status().isOk());
+    }
+}
